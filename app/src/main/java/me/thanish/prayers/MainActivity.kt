56575d1c728298eb4cf521dd5c560dbb681d11f8
Muhammed Thanish <mnmtanish@gmail.com>
2025-01-02 13:44:56 +0100

    chore: initialize workers async to speed up startup

--- app/src/main/java/me/thanish/prayers/MainActivity.kt
+++ app/src/main/java/me/thanish/prayers/MainActivity.kt
@@ -1,13 +1,12 @@
 package me.thanish.prayers
 
-import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import me.thanish.prayers.theme.PrayersTheme
 import me.thanish.prayers.worker.NotificationWorker
 import me.thanish.prayers.worker.SchedulerWorker
@@ -19,8 +18,11 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        SchedulerWorker.initialize(applicationContext)
-        NotificationWorker.initialize(applicationContext)
+        // Initialize workers asynchronously
+        CoroutineScope(Dispatchers.IO).launch {
+            SchedulerWorker.initialize(applicationContext)
+            NotificationWorker.initialize(applicationContext)
+        }
 
         enableEdgeToEdge()
         setContent {
