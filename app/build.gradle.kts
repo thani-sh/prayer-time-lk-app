222c754bcc058fd9e8b7def60f98d3de6cdf805d
Muhammed Thanish <mnmtanish@gmail.com>
2025-01-17 14:37:45 +0100

    chore: bump the app version to v3.9 (21)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "me.thanish.prayers"
         minSdk = 26
         targetSdk = 35
-        versionCode = 20
-        versionName = "3.8"
+        versionCode = 21
+        versionName = "3.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
