953ea9981addd1825ce1f3f9129ffe8c0444b2f7
Muhammed Thanish <mnmtanish@gmail.com>
2025-02-14 01:10:13 +0100

    feat: allow adjusting the hijri date if needed

--- apps/android/app/src/main/java/me/thanish/prayers/routes/settings/SettingsRoute.kt
+++ apps/android/app/src/main/java/me/thanish/prayers/routes/settings/SettingsRoute.kt
@@ -15,9 +15,7 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.outlined.Settings
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -26,23 +24,24 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import me.thanish.prayers.R
 import me.thanish.prayers.device.DEBUG_ENABLED
 import me.thanish.prayers.device.RequestPermission
+import me.thanish.prayers.domain.HijriCalendarOffset
 import me.thanish.prayers.domain.NotificationOffset
 import me.thanish.prayers.domain.PrayerTimeCity
 import me.thanish.prayers.domain.PrayerTimeMethod
 import me.thanish.prayers.routes.RouteSpec
 import me.thanish.prayers.routes.RouteType
 import me.thanish.prayers.routes.settings.components.FeedbackIconButtons
+import me.thanish.prayers.routes.settings.components.HijriCalendarOffsetSlider
 import me.thanish.prayers.routes.settings.components.SelectCityDropdown
 import me.thanish.prayers.routes.settings.components.SelectMethodDropdown
-import me.thanish.prayers.routes.settings.components.SelectOffsetDropdown
+import me.thanish.prayers.routes.settings.components.SelectOffsetSlider
+import me.thanish.prayers.routes.settings.components.SelectOffsetToggle
 import me.thanish.prayers.routes.settings.components.SettingsSectionWithTitle
 import me.thanish.prayers.routes.settings.components.TestNotificationButton
 import me.thanish.prayers.theme.PrayersTheme
@@ -66,6 +65,7 @@ fun SettingsRoute(nav: NavController, modifier: Modifier = Modifier) {
     var city by remember { mutableStateOf(PrayerTimeCity.get(context)) }
     var method by remember { mutableStateOf(PrayerTimeMethod.get(context)) }
     var offset by remember { mutableStateOf(NotificationOffset.get(context)) }
+    var hijriOffset by remember { mutableStateOf(HijriCalendarOffset.get(context)) }
 
     val onCityChange = { selectedCity: PrayerTimeCity ->
         city = selectedCity
@@ -89,6 +89,11 @@ fun SettingsRoute(nav: NavController, modifier: Modifier = Modifier) {
         if (NotificationOffset.isEnabled(context)) {
             SchedulerWorker.schedule(nav.context, method, city)
         }
+    }
+
+    val onHijriOffsetChange = { selectedOffset: HijriCalendarOffset ->
+        hijriOffset = selectedOffset
+        HijriCalendarOffset.set(context, selectedOffset)
     }
 
     val onTestNotification = { delay: Long ->
@@ -120,6 +125,8 @@ fun SettingsRoute(nav: NavController, modifier: Modifier = Modifier) {
         onMethodChange,
         offset,
         onOffsetChange,
+        hijriOffset,
+        onHijriOffsetChange,
         onTestNotification,
         modifier
     )
@@ -133,6 +140,8 @@ fun SettingsRouteView(
     onMethodChange: (PrayerTimeMethod) -> Unit,
     offset: NotificationOffset,
     onOffsetChange: (NotificationOffset) -> Unit,
+    hijriOffset: HijriCalendarOffset,
+    onHijriOffsetChange: (HijriCalendarOffset) -> Unit,
     onTestNotification: (Long) -> Unit,
     modifier: Modifier = Modifier
 ) {
@@ -148,22 +157,21 @@ fun SettingsRouteView(
         ) {
             SelectCityDropdown(city, onCityChange)
             SelectMethodDropdown(method, onMethodChange)
-
-            if (method.getDescription(LocalContext.current) != null) {
-                Text(
-                    text = method.getDescription(LocalContext.current)!!,
-                    fontSize = 11.sp,
-                    fontStyle = FontStyle.Italic,
-                    color = MaterialTheme.colorScheme.onSurfaceVariant
-                )
-            }
         }
 
         SettingsSectionWithTitle(
             titleText = stringResource(R.string.route_settings_section_notifications),
             descriptionText = stringResource(R.string.route_settings_section_notifications_details),
         ) {
-            SelectOffsetDropdown(offset, onOffsetChange)
+            SelectOffsetToggle(offset, onOffsetChange)
+            SelectOffsetSlider(offset, onOffsetChange)
+        }
+
+        SettingsSectionWithTitle(
+            titleText = stringResource(R.string.route_settings_section_corrections),
+            descriptionText = stringResource(R.string.route_settings_section_corrections_details),
+        ) {
+            HijriCalendarOffsetSlider(hijriOffset, onHijriOffsetChange)
         }
 
         SettingsSectionWithTitle(
@@ -185,9 +193,11 @@ fun SettingsRoutePreview() {
     val city = PrayerTimeCity.colombo
     val method = PrayerTimeMethod.shafi
     val offset = NotificationOffset(10)
+    val hijriOffset = HijriCalendarOffset(0)
     val onCityChange: (PrayerTimeCity) -> Unit = {}
     val onMethodChange: (PrayerTimeMethod) -> Unit = {}
     val onOffsetChange: (NotificationOffset) -> Unit = {}
+    val onHijriOffsetChange: (HijriCalendarOffset) -> Unit = {}
     val onTestNotification: (Long) -> Unit = {}
 
     PrayersTheme {
@@ -199,6 +209,8 @@ fun SettingsRoutePreview() {
                 onMethodChange,
                 offset,
                 onOffsetChange,
+                hijriOffset,
+                onHijriOffsetChange,
                 onTestNotification,
                 modifier = Modifier.padding(innerPadding)
             )
